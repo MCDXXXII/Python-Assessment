3445d100d68585da2762598486fcb8ad00f8fedf
Caleb Eubanks <116412148+Fourteenthirtytoo@users.noreply.github.com>
2023-06-17 09:02:04 -0400

    Update file_system.py

--- file_system.py
+++ file_system.py
@@ -19,9 +19,9 @@ while not initialize:
         initialize = True
 
 #CODE BELOW MUST REMAIN CONSISTENT THROUGHOUT FUNCTIONS
-home_dir = (rf'C:\users\{username}')
-current_dir_title = home_dir.split('\\')[-1]
-current_dir = {f'{home_dir}': ['downloads', 'pictures', 'documents', 'music'],
+current_folder = (rf'C:\users\{username}')
+current_folder_title = current_folder.split('\\')[-1]
+file_system = {f'{current_folder}': ['downloads', 'pictures', 'documents', 'music'],
                 rf'C:\users\{username}\downloads': ['example_file.txt'],
                 rf'C:\users\{username}\pictures': ['example_file.txt'],
                 rf'C:\users\{username}\documents': ['example_file.txt'],
@@ -29,31 +29,29 @@ current_dir = {f'{home_dir}': ['downloads', 'pictures', 'documents', 'music'],
                }
 
 #DIRECTORIES IS ONLY FOR REFERENCE
-directories = {'home': ['downloads',
-                       'pictures',
-                       'documents',
-                       'music',]
-                       }
+# directories = {'home': ['downloads',
+#                        'pictures',
+#                        'documents',
+#                        'music',]
+#                        }
 
-starting_dir = (rf'C:\users\{username} > ')
+# starting_dir = (rf'C:\users\{username} > ')
 
-program_count = 0
 
 #BELOW CODE WILL BE FUNCTIONS LS, MKDIR, CD, AND TOUCH,
 def ls():
-    global home_dir
-    for folder in current_dir[home_dir]:
+    global current_folder
+    for folder in file_system[current_folder]:
         print(folder)
-    
+
+
 def mkdir():
-    global home_dir
+    global current_folder
     example = ''
     
     new_folder_title = argument.split(' ')[1].lower()
-    new_folder = f'{home_dir}\{new_folder_title}'
-    if argument == 'mkdir':
-        print('mkdir Usage: mkdir requires a folder name after the command is called')
-    elif len(argument.split(' ')) > 2:
+    new_folder = f'{current_folder}\{new_folder_title}'
+    if len(argument.split(' ')) > 2:
         print(f'mkdir Usage: Input "{argument}" invalid. folder name must be one position. Use "_" to concatenate')
         example_list = argument.split(' ')
         for i in range(1, len(example_list)):
@@ -61,32 +59,30 @@ def mkdir():
             if i + 1 < len(example_list):
                 example += '_'
         print(f'>>>>>>>>>>> example folder name: {example}')
-    elif new_folder in current_dir:
+    elif new_folder in file_system:
         print('mkdir Usage: Folder already exists')
             
     else:
-        current_dir[f'{new_folder}'] = []
-        current_dir[f'{home_dir}'].append(new_folder_title)
+        file_system[f'{new_folder}'] = []
+        file_system[f'{current_folder}'].append(new_folder_title)
     
 def cd():
-    global home_dir
+    global current_folder
     if argument == 'cd':
-        home_dir = ' '
-        home_dir = (rf'C:\users\{username}')
-    elif argument.split(' ')[1].lower() in current_dir[home_dir]:
+        current_folder = ' '
+        current_folder = (rf'C:\users\{username}')
+    elif argument.split(' ')[1].lower() in file_system[current_folder]:
         change_dir_to = argument.split(' ')[1].lower()
-        home_dir += f'\{change_dir_to}'
+        current_folder += f'\{change_dir_to}'
     else:
         change_dir_to = argument.split(' ')[1].lower()
         print(rf'cd: Cannot find path C:users\{username}\{change_dir_to} because it does not exist')
 
 def touch():
-    global home_dir
+    global current_folder
     example = ''
     new_file = argument.split(' ')[1].lower() + '.txt'
-    if argument == 'touch':
-        print('touch Usage: touch requires a file name after the command is called')
-    elif len(argument.split(' ')) > 2:
+    if len(argument.split(' ')) > 2:
         print(f'touch Usage: Input "{argument}" invalid. File name must be one position. Use "_" to concatenate')
         example_list = argument.split(' ')
         for i in range(1, len(example_list)):
@@ -94,24 +90,34 @@ def touch():
             if i + 1 < len(example_list):
                 example += '_'
         print(f'>>>>>>>>>>> example file name: {example}')
-    elif new_file in current_dir[home_dir]:
+    elif new_file in file_system[current_folder]:
         print(f'{new_file} Overwritten')
     else:
-        current_dir[home_dir].append(new_file)
+        file_system[current_folder].append(new_file)
 
 #BELOW CODE WILL KEEP PROGRAM IN LOOP TO MANIPULATE DIRECTORIES UNTIL EXIT 
 while program_run:
-    program_count += 1
-    argument = input(f'\n{home_dir} > ').lower()
-    if 'ls' in argument:
-        ls()
-    elif 'mkdir' in argument:
-        mkdir()
-    elif 'cd' in argument:
+    
+    argument = input(f'\n{current_folder} > ').lower()
+    argument_split = argument.split()
+    if argument_split[0] == 'ls':
+        if len(argument_split) == 1:
+            ls()
+        else:
+            print(f'Command not found: {argument}')
+    elif argument_split[0] == 'mkdir':
+        if len(argument_split) > 1:
+            mkdir()
+        else:
+            print('mkdir Usage: mkdir requires a folder name after the command is called')
+    elif argument_split[0] == 'cd':
         cd()
-    elif 'touch' in argument:
-        touch()
-    elif argument == 'exit':
+    elif argument_split[0] == 'touch':
+        if len(argument_split) > 1:
+            touch()
+        else:
+            print('touch Usage: touch requires a file name after the command is called')
+    elif argument_split[0] == 'exit':
         program_run = False
     else:
         print(f'Command not found: {argument}')
